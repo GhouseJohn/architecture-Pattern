27530bf16b88c75b1cebf6b97782cc45ddb86e6b
irfanulla m <irfanm.official@outlook.com>
2019-09-25 18:54:55 +0530

    Added ViewModel Class

---
+++ Common/BusinessModels/ViewModel/CustomerBillingModel.cs
@@ -0,0 +1,17 @@
+﻿using BusinessModels.CGC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessModels.ViewModel
+{
+    public class CustomerBillingModel
+    {
+        public MasterTable _masTable { get; set; }
+        public IEnumerable<MasterTable> _GetMasterData { get; set; }
+
+
+}
+}
